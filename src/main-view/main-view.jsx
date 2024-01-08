f2682aa1facc78426fc009a596d3be6f05a5363a
Alexis Abner <alexisabner@alexiss-air.lan>
2024-01-08 16:43:09 -0700

    main-view folder added into src folder

---
+++ src/main-view/main-view.jsx
@@ -0,0 +1,10 @@
+//MainView component created. It acts as the homepage of the app.
+export const MainView = () => {
+    return (
+        <div>
+            <div>The Lord of the Rings: The Return of the King</div>
+            <div>Pulp Fiction</div>
+            <div>Silence of the Lambs</div>
+        </div>
+    );
+}
