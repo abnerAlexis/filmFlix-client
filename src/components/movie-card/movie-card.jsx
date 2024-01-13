d8fef108f842a098fe0cf01ec945534bb9c31314
Alexis Abner <alexisabner@alexiss-air.lan>
2024-01-12 20:16:26 -0700

    Add prop-types specifcation to movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,7 @@
+//importing proto-types
+import PropTypes from "prop-types";
+
+//MovieCard function component
 export const MovieCard = ({movie, onMovieClick}) => {
   return (
     <div
@@ -8,4 +12,19 @@ export const MovieCard = ({movie, onMovieClick}) => {
         {movie.title}
     </div>
   );
+};
+
+//Prop constrains' definitions for the MovieCard
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Year: PropTypes.number,
+    Image: PropTypes.string,
+    Genre: PropTypes.string,
+    Featured: PropTypes.bool,
+    Description: PropTypes.string,
+    Director: PropTypes.array,
+    Actors: PropTypes.array,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
