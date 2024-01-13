2ae67f56949d3dd39df7fcc02d75b512607dc227
Alexis Abner <alexisabner@alexiss-air.lan>
2024-01-12 18:59:31 -0700

    Replace hardcoded movies array list with an empty array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,38 +4,7 @@ import { MovieView } from "../movie-view/movie-view";
 
 //MainView component created. It acts as the homepage of the app.
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1, 
-            title: "The Lord of the Rings: The Return of the King",
-            genre: "Adventure",
-            imageURL: "https://m.media-amazon.com/images/I/91LAfx+AZhL._AC_UY436_FMwebp_QL65_.jpg",
-        },
-        {
-            id: 2, 
-            title: "Pulp Fiction",
-            genre: "Crime",
-            imageURL: "https://m.media-amazon.com/images/I/91hqWdWIN+L._AC_UY436_FMwebp_QL65_.jpg"
-        },
-        {
-            id: 3, 
-            title: "Silence of the Lambs",
-            genre: "Crime",
-            imageURL: "https://m.media-amazon.com/images/I/71G5bef7vZL._AC_UY436_FMwebp_QL65_.jpg"
-        },
-        {
-            id: 4, 
-            title: "The Good, the Bad and the Ugly",
-            genre: "Western",
-            imageURL: "https://m.media-amazon.com/images/I/91BDDsP0clL._AC_UY436_FMwebp_QL65_.jpg",
-        },
-        {
-            id: 5, 
-            title: "Schindler's List",
-            genre: "Drama",
-            imageURL: "https://m.media-amazon.com/images/I/81pBGFSSSwL._AC_UY436_FMwebp_QL65_.jpg",
-    },
-    ]);
+    const [movies, setMovies] = useState([]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
 
