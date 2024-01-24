d9639c8f8f0744a6dd6a8a3684769b809ac0cce6
Alexis Abner <alexisabner@alexiss-air.lan>
2024-01-23 18:24:50 -0700

    Form elements added in login-view.jsx replacing the previous return cont ents

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { Button, FormGroup } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
 
 export const LoginView = ({ onLoggedIn }) => {
