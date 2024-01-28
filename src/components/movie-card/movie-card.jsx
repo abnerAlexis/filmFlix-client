90e58c3e11f9e0efafb3ac6688991ed1a4cc3d40
Alexis Abner <alexisabner@alexiss-air.lan>
2024-01-27 18:56:00 -0700

    Remove onMovieClick from movie-card and encodeURIComponent is called with book id

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -5,6 +5,36 @@ import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 //MovieCard function component
+export const MovieCard = ({ movie }) => {
+  return (
+    <Card>
+      <Card.Img variant="top" src={movie.Image} /> 
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Genre}</Card.Text>
+        <Link to={`/movies/${encodeURIComponent(movie._id)}`} >
+          <Button variant="link">Open</Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
+};
+
+//Prop constrains' definitions for the MovieCard
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Year: PropTypes.number,
+    Image: PropTypes.string,
+    Genre: PropTypes.string,
+    Featured: PropTypes.string,
+    Description: PropTypes.string,
+    Director: PropTypes.string,
+    Actors: PropTypes.string,
+  }).isRequired,
+};
+/*
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <Card className="h-100" onClick={() => onMovieClick(movie)}>
@@ -23,17 +53,4 @@ export const MovieCard = ({ movie, onMovieClick }) => {
   );
 };
 
-//Prop constrains' definitions for the MovieCard
-MovieCard.propTypes = {
-  movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
-    Year: PropTypes.number,
-    Image: PropTypes.string,
-    Genre: PropTypes.string,
-    Featured: PropTypes.string,
-    Description: PropTypes.string,
-    Director: PropTypes.string,
-    Actors: PropTypes.string,
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
-};
+*/
