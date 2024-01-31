c2c53f4676bcd619c72bf93d2424029a16643824
Alexis Abner <alexisabner@alexiss-air.lan>
2024-01-30 17:31:45 -0700

    Create ProfileView, update MainView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,8 +4,7 @@ import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 import { NavigationBar } from "../navigation-bar/navigation-bar"
-import { Row } from "react-bootstrap";
-import { Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 //MainView component created. It acts as the homepage of the app.
---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+export const ProfileView = ({user, onUserUpdate, onDeregister}) => {
+
+}
