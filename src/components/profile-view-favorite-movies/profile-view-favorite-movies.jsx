52795d9808ba5f3dec79a24de3a29900a796801c
Alexis Abner <alexisabner@alexiss-air.lan>
2024-02-04 08:27:16 -0700

    Create favorite movies component

---
+++ src/components/profile-view-favorite-movies/profile-view-favorite-movies.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { Card, Button } from "react-bootstrap";
+
+import React from 'react'
+
+export default function ProfileViewFavoriteMovies
+() {
+  return (
+    <div>ProfileViewFavoriteMovies
+        
+    </div>
+  )
+}
