c9eb52676b1129fedd62d2c9cee5a78db45e1489
Alexis Abner <alexisabner@alexiss-air.lan>
2024-02-26 14:07:45 -0700

    Create search-bar.jsx in navigation-bar folder.

---
+++ src/components/navigation-bar/search-bar.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export const SearchBar = ( {onSearch }) => {
+    const [query, setQuery] = useState("");
+    
+    const handleSearch = () => {
+        onSearch(query);
+    }
+
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Search movies by title..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
+        </div>
+    );
+}
