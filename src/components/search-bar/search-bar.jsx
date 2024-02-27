fa1b88877ee80f11cf8038b76c3e8a8f12d9b72e
Alexis Abner <alexisabner@alexiss-air.lan>
2024-02-27 13:11:15 -0700

    add search-bar component

---
+++ src/components/search-bar/search-bar.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+
+export const SearchBar = ({onSearch}) => {
+    const [query, setQuery] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSearch(query);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Search by title..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
+            <button style={{marginLeft:10}} type="submit">Search</button>
+        </form>
+    )
+}
