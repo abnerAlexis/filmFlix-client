21b3b832499705559b4c5c0c0142f6812861720f
Alexis Abner <alexisabner@alexiss-air.lan>
2024-04-08 10:31:39 -0600

    Add atatus

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,10 @@ import Container from "react-bootstrap";
 import "./index.scss";
 import { Container } from "react-bootstrap";
 
+import * as atatus from 'atatus-spa';
+atatus.config('2d125e18d8e948a49a4a6ef155804edf').install();
+
+
 //Main component (will eventually use all the others)
 const App = () => {
   return (
@@ -19,3 +23,4 @@ const root = createRoot(container);
 
 // Tells React to render your app in the root DOM element
 root.render(<App />);
+atatus.notify(new Error('Test Atatus Setup'));
